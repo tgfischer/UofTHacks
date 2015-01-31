32b3b5c4d1b774c724055ff77bc6034a9376a0ac
David Nisenbaum <dnisenbaum1@gmail.com>
2015-01-31 18:10:58 -0500

    pagination works

--- scripts/script.js
+++ scripts/script.js
@@ -1,139 +1,179 @@
 $(document).ready(function() {
-  	$("#total-results").hide();
-  	$(".hide").hide();
+  var currentPage;
+  var totalPages;
+
+	$(".hide").hide();
 
 	$("#searchButton").click(function() {
       $("#searchForm").submit(); 
 	});
 
 	$('#searchItem').keydown(function(event) {
-        if (event.keyCode == 13) {
-            $("#searchForm").submit(); 
-            return false;
-         }
-    });
+    if (event.keyCode == 13) {
+        $("#searchForm").submit(); 
+        return false;
+     }
+  });
 
-    $('#searchCity').keydown(function(event) {
-        if (event.keyCode == 13) {
-            $("#searchForm").submit(); 
-            return false;
-         }
-    });
+  $('#searchCity').keydown(function(event) {
+      if (event.keyCode == 13) {
+          $("#searchForm").submit(); 
+          return false;
+       }
+  });
 
-  	$("#searchForm").submit(function(e) {
-  		// Fix sql injection threat;
-  		var city = capitaliseFirstLetter($('#searchCity').val().toLowerCase());
-  		var url = "https://api.sqoot.com/v2/deals?api_key=6vc2ns&query=" + $('#searchItem').val() + "&location=" + city;
-  		console.log(url);
+	$("#searchForm").submit(function(e) {
+		// Fix sql injection threat;
+		var city = capitaliseFirstLetter($('#searchCity').val().toLowerCase());
+		var url = "https://api.sqoot.com/v2/deals?api_key=6vc2ns&query=" + $('#searchItem').val() + "&location=" + city;
+		currentPage = 1;
 
-  		$.ajax({
-  			type : "GET",
-  			url : url,
-  			dataType : "jsonp",
-  			success : function(data) {
-  				$("#information").empty();
-  				$("#information").slideUp("fast");
-  				$("#no-results").slideUp("fast");
+    pageLoad(url, city, currentPage);
+	
+  	return false;
+	});
 
-  				if (data.deals.length == 0 || (city && !data.query.location.locality)) {
-  					$("#no-results").slideDown("fast");
-  					$(".hide").slideUp("fast");
-  					return;
-  				}
+  function pageLoad (url, city, currentPage) {
+    $.ajax({
+      type : "GET",
+      url : url,
+      dataType : "jsonp",
+      success : function(data) {
+        $("#information").empty();
+        $("#information").slideUp("fast");
+        $("#no-results").slideUp("fast");
 
-  				$(".hide").slideDown("fast");
-      		var deals = data.deals;
-          var info = data.query;
+        if (data.deals.length == 0 || (city && !data.query.location.locality)) {
+          $("#no-results").slideDown("fast");
+          $(".hide").slideUp("fast");
+          return;
+        }
 
-          var pages = Math.ceil(Number(info.total)/10);
-          console.log(pages);
-          $("#search-results").empty();
-          $("#search-results").append("Total Results - "+info.total); 
+        $(".hide").slideDown("fast");
+        var deals = data.deals;
+        var info = data.query;
 
-      		deals.forEach(function (deal) {
-            console.log(deal);
+        totalPages = Math.ceil(Number(info.total)/10);
 
-        		$("#information").append("<div class='title'> \
-        								  	<i class='dropdown icon'></i>" + 
-        								  	deal.deal.title + 
-        								 "</div> \
-        								 <div class='content'> \
-        								 	<div class='ui middle aligned grid'>" +
-        								 		(deal.deal.description == null ? "" : "<div class='row'> \
-        								 			<div class='column'> \
-        								 				<p>" + deal.deal.description + "</p> \
-        								 			</div> \
-        								 		</div>") + 
-        								 		"<div class='equal height row'> \
-        								 			<div class='six wide column'> \
-        								 				<img class='ui rounded image' src='" + deal.deal.image_url + "' /> \
-        								 			</div> \
-        								 			<div class='ten wide column'> \
-										 			<table class='ui unstackable table'> \
-										 				<thead> \
-										 					<tr> \
-										 						<th colspan='2'>Contact Information</th> \
-										 					</tr> \
-										 				</thead> \
-										 				<tbody> \
-										 					<tr> \
-										 						<td class='collapsing'>Address</td> \
-										 						<td> \
-										 							<addr>" + 
-										 								(deal.deal.merchant.address == null ? "None Provided" : 
-										 								deal.deal.merchant.address + "<br>" + 
-										 								deal.deal.merchant.locality + " " + deal.deal.merchant.region + ", " + deal.deal.merchant.country_code + "<br>" +
-										 								deal.deal.merchant.postal_code) + " \
-										 							</addr> \
-										 						</td> \
-										 					</tr> \
-										 					<tr> \
-										 						<td class='collapsing'>Phone</td> \
-										 						<td>" + (deal.deal.merchant.phone_number == null ? "None Provided" : deal.deal.merchant.phone_number) + "</td> \
-										 					</tr> \
-										 				</tbody> \
-										 			</table> \
-										 		</div> \
-        										</div>" + (deal.deal.fine_print == null ? "" :
-        										"<div class='row'> \
-        											<div class='column'> \
-        												<i>" + deal.deal.fine_print + "</i> \
-        											</div> \
-        										</div>") +
-        										"<div class='row'> \
-        											<div class='column'> \
-        								 				<div id='" + deal.deal.url + "' class='ui fluid teal button'>" + 
-        								 					deal.deal.provider_name + " \
-        								 				</div> \
-        								 			</div> \
-        								 		</div> \
-        								 	</div> \
-        								 </div>");
+        $("#page-number").empty();
+        $("#page-number").append(currentPage + " / " + totalPages);
+        $("#search-results").empty();
+        $("#search-results").append("Total Results - "+info.total); 
 
-        		var link = document.getElementById(deal.deal.url);
+        deals.forEach(function (deal) {
+          //console.log(deal);
 
-        		link.addEventListener('click', function() {
-          			newTab(deal.deal.url);
-        		});
-      		});
-          $('html').css({ height : "auto" });
-        } //end of success
-    	}); //end of ajax
-  	
-    	return false;
-  	});
+          $("#information").append("<div class='title'> \
+                          <i class='dropdown icon'></i>" + 
+                          deal.deal.title + 
+                       "</div> \
+                       <div class='content'> \
+                        <div class='ui middle aligned grid'>" +
+                          (deal.deal.description == null ? "" : "<div class='row'> \
+                            <div class='column'> \
+                              <p>" + deal.deal.description + "</p> \
+                            </div> \
+                          </div>") + 
+                          "<div class='equal height row'> \
+                            <div class='six wide column'> \
+                              <img class='ui rounded image' src='" + deal.deal.image_url + "' /> \
+                            </div> \
+                            <div class='ten wide column'> \
+                        <table class='ui unstackable table'> \
+                          <thead> \
+                            <tr> \
+                              <th colspan='2'>Contact Information</th> \
+                            </tr> \
+                          </thead> \
+                          <tbody> \
+                            <tr> \
+                              <td class='collapsing'>Address</td> \
+                              <td> \
+                                <addr>" + 
+                                  (deal.deal.merchant.address == null ? "None Provided" : 
+                                  deal.deal.merchant.address + "<br>" + 
+                                  deal.deal.merchant.locality + " " + deal.deal.merchant.region + ", " + deal.deal.merchant.country_code + "<br>" +
+                                  deal.deal.merchant.postal_code) + " \
+                                </addr> \
+                              </td> \
+                            </tr> \
+                            <tr> \
+                              <td class='collapsing'>Phone</td> \
+                              <td>" + (deal.deal.merchant.phone_number == null ? "None Provided" : deal.deal.merchant.phone_number) + "</td> \
+                            </tr> \
+                          </tbody> \
+                        </table> \
+                      </div> \
+                          </div>" + (deal.deal.fine_print == null ? "" :
+                          "<div class='row'> \
+                            <div class='column'> \
+                              <i>" + deal.deal.fine_print + "</i> \
+                            </div> \
+                          </div>") +
+                          "<div class='row'> \
+                            <div class='column'> \
+                              <div id='" + deal.deal.url + "' class='ui fluid teal button'>" + 
+                                deal.deal.provider_name + " \
+                              </div> \
+                            </div> \
+                          </div> \
+                        </div> \
+                       </div>");
 
-  	$('.ui.accordion').accordion();
+          var link = document.getElementById(deal.deal.url);
 
-  	$('.title').click(function() {
-  		$('html').css({ height : "auto" });
-  	});
+          link.addEventListener('click', function() {
+              newTab(deal.deal.url);
+          });
+        });
+        $('html').css({ height : "auto" });
+      } //end of success
+    }); //end of ajax
+  }
 
-  	function newTab(url) {
-    	chrome.tabs.create({ url: url });
-  	}
-
-  	function capitaliseFirstLetter(string) {
-    	return string.charAt(0).toUpperCase() + string.slice(1);
+	function newTab(url) {
+  	chrome.tabs.create({ url: url });
 	}
+
+	function capitaliseFirstLetter(string) {
+  	return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  function goNext() {
+    currentPage = currentPage + 1
+    var city = capitaliseFirstLetter($('#searchCity').val().toLowerCase());
+    var url = "https://api.sqoot.com/v2/deals?api_key=6vc2ns&query=" + $('#searchItem').val() + "&location=" + city + "&page=" + currentPage;
+
+    pageLoad(url, city, currentPage);
+  
+  }
+
+  function goBack() {   
+    currentPage = currentPage - 1
+    var city = capitaliseFirstLetter($('#searchCity').val().toLowerCase());
+    var url = "https://api.sqoot.com/v2/deals?api_key=6vc2ns&query=" + $('#searchItem').val() + "&location=" + city + "&page=" + currentPage;
+
+    pageLoad(url, city, currentPage);
+  }
+
+  $("#next-page").click(function() {
+    if (currentPage == 1) {
+      return
+    }
+    goNext();
+  });
+
+  $("#prev-page").click(function() {
+    if (currentPage == 1) {
+      return
+    }
+    goBack();
+  });
+
+  $('.ui.accordion').accordion();
+
+  $('.title').click(function() {
+    $('html').css({ height : "auto" });
+  });
+
 });
